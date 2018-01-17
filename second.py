53e4aa14c9f1bb1038d95675baee3125d6d16031
Song Myoung Su <audtn0508@naver.com>
2018-01-17 23:00:56 +0900

    add

--- second.py
+++ second.py
@@ -13,3 +13,16 @@ print(a and b)
 
 # A가 참이고 그리고 B가 참이라면 (A와 B가 둘다 참이여야 된다.)
 print(a or b)
+
+# = & ==
+a = True
+print(a == True)
+print(a is True)
+
+d = 7
+if d > 10:
+    print("숫자는 10보다 큽니다.")
+elif d > 5 and d <= 10:
+    print("숫자는 10보다 작거나 같고, 5보다는 큽니다.")
+else:
+    print("숫자는 5보다 작거나 같습니다.")
